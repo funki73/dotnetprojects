18328a732196dae3e304fe35a0ace2fe36f43d00
funki73 <randy.calpe@hotmail.com>
2021-11-29 06:52:47 -0800

    first commit

---
+++ practice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args){
+            //The execution section does not always have to use ++
+            for (int i = 1; i < 6; i = i + 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
