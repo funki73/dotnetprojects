f4cd3d18c3865cef8c9add19845d53f3706386ac
funki73 <randy.calpe@hotmail.com>
2021-11-29 09:11:52 -0800

    1129210911

--- fundamentals1/Program.cs
+++ fundamentals1/Program.cs
@@ -6,7 +6,16 @@ namespace fundamentals1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create a Loop that prints all values from 1-255
+            // for (int i = 1; i < 256; i = i + 1)
+            // {
+            //     Console.WriteLine(i);
+            // }
+            // Create a new loop that prints all values from 1-100 that are divisible by 3 or 5, but not both
+            for (int i = 1; i < 101; i = i +1){
+                Console.WriteLine(i);
+            else if ()
+            }
         }
     }
 }
