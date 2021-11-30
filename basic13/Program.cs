128dad9c8f17647307d041d7909c114db992b169
funki73 <randy.calpe@hotmail.com>
2021-11-30 04:38:25 -0800

    1130210438

---
+++ basic13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace basic13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public static void PrintNumbers()
+            {
+                for (int i = 1; i < 256; i = i + 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
