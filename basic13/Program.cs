9742fca465037545d363b24aa035afa9aa842b6a
funki73 <randy.calpe@hotmail.com>
2021-11-30 14:58:09 -0800

    1130210258

--- basic13/Program.cs
+++ basic13/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace basic13
 {
@@ -10,8 +11,12 @@ namespace basic13
             // PrintNumbers();
             // PrintOdds();
             // PrintSum();
-            LoopArray(new int[] {1, 2, 3});
-            
+            // LoopArray(new int[] {1, 2, 3});
+            // FindMax(new int[] {-3, 4, 0, 9, 15});
+            // GetAverage(new int[] {2, 10, 3});
+            // OddArray();
+            // GreaterThanY(new int[] {1, 3, 5, 7}, 3);
+                SquareArrayValues(new int[] {1, 5, 10, -10});
         }
         public static void PrintNumbers()
         {
@@ -49,13 +54,85 @@ namespace basic13
             }
         }
         public static void LoopArray(int[] numbers)
-            {
+        {
             // Write a function that would iterate through each item of the given integer array and 
             // print each value to the console. 
             for (int i = 0; i < numbers.Length; i++)
             {
                 Console.WriteLine(numbers[i]);
             }
+        }
+        public static int FindMax(int[] numbers)
+        {
+            // Write a function that takes an integer array and prints and returns the maximum value in the array. 
+            // Your program should also work with a given array that has all negative numbers (e.g. [-3, -5, -7]), 
+            // or even a mix of positive numbers, negative numbers and zero.
+            int max = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
             }
+                    Console.WriteLine(max);
+                    return max;
+        }
+        public static void GetAverage(int[] numbers)
+        {
+            // Write a function that takes an integer array and prints the AVERAGE of the values in the array.
+            // For example, with an array [2, 10, 3], your program should write 5 to the console.
+            int sum = 0;
+            int avg = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers [i];
+            }
+                avg = sum / numbers.Length;
+                Console.WriteLine(avg);
+        }
+        public static int[] OddArray()
+        {
+            // Write a function that creates, and then returns, an array that contains all the odd numbers between 1 to 255. 
+            // When the program is done, this array should have the values of [1, 3, 5, 7, ... 255].
+            List<int> newList = new List<int>();
+            for (int i = 1; i <= 255; i++)
+            {
+                if(i % 2 == 1)
+                {
+                    newList.Add(i);
+                }
+            }
+            int[] answerArr = newList.ToArray();
+            System.Console.WriteLine("[{0}]", string.Join(", ", answerArr));
+            return answerArr;
+        }
+        public static int GreaterThanY(int[] numbers, int y)
+        {
+            // Write a function that takes an integer array, and a integer "y" and returns the number of array values 
+            // That are greater than the "y" value. 
+            // For example, if array = [1, 3, 5, 7] and y = 3. Your function should return 2 
+            // (since there are two values in the array that are greater than 3).
+            int greater = 0;
+            foreach (int number in numbers)
+            {
+                if(number > y)
+                {
+                    greater++;
+                }
+            }
+            Console.WriteLine(greater);
+            return greater;
+        }
+        public static void SquareArrayValues(int[] numbers)
+        {
+            // Write a function that takes an integer array "numbers", and then multiplies each value by itself.
+            // For example, [1,5,10,-10] should become [1,25,100,100]
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = numbers[i] * numbers[i];
+            }
+            Console.WriteLine(numbers);
+        }
     }
 }
