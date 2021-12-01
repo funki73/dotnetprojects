0f5447430c1c5f7e5152405094dbc221309b3d56
funki73 <randy.calpe@hotmail.com>
2021-11-30 21:12:03 -0800

    1130210912

--- puzzles/Program.cs
+++ puzzles/Program.cs
@@ -9,7 +9,8 @@ namespace puzzles
         {
             // Console.WriteLine("Hello World!");
             // RandomArray();
-            TossCoin();
+            // TossCoin();
+            var result = Names();
         }
 
         public static void RandomArray()
@@ -43,37 +44,70 @@ namespace puzzles
             //Randomize a coin toss with a result signaling either side of the coin 
             //Have the function print either "Heads" or "Tails"
             //Finally, return the result
-        public static void TossCoin()
+        public static string TossCoin()
         {
         Console.WriteLine("Tossing a Coin!");
-        Random ran = new Random();
-        int randNum = ran.Next();
-        if(randNum == 1)
         {
-            Console.WriteLine("Heads");
-        }
-        else if(randNum == 2)
-        {
-            Console.WriteLine("Tails");
+        Random rand = new Random();
+        string result;
+
+            if(rand.Next(2) == 0)
+                result = "Heads";
+            else
+                result = "Tails";
+
+            Console.WriteLine($"The result is: {result}");
+            return result;
         }
         }
 
         //Create another function called TossMultipleCoins(int num) that returns a Double
             //Have the function call the tossCoin function multiple times based on num value
             //Have the function return a Double that reflects the ratio of head toss to total toss
-
-
+        public static double TossMultiple(int numTimes)
+        {
+            Random rand = new Random();
+            int numHeads = 0;
+            for(var toss = 0; toss < numTimes; toss++)
+            {
+                if(rand.Next(2) == 0)
+                    numHeads++;
+            }
+            return (double)numHeads/numTimes;
+        }
 
         //Build a function Names that returns a list of strings.//
             //Create a list with the values: Todd, Tiffany, Charlie, Geneva, Sydney
             //Shuffle the list and print the values in the new order
             //Return a list that only includes names longer than 5 characters
-
-        List<string> thenames = new List<string>();
-        thenames.Add("Todd");
-        thenames.Add("Tiffany")
-        thenames.Add("Charlie")
-        thenames.Add("Genove")
-        thenames.Add("Sydney")
+        public static List<string> Names()
+        {
+        List<string> names = new List<string>()
+        {
+            "Todd", "Tiffany", "Charlie", "Geneva", "Sydney"
+        };
+        Random rand = new Random();
+        // shuffle names
+        for(var i = 0; i<names.Count/2; i++)
+        {
+            // swap names[i] with names[randomIndex]
+            int randomIndex = rand.Next(names.Count);
+            string temp = names[randomIndex];
+            names[randomIndex] = names[i];
+            names[i] = temp;
+        }
+        // print new order of names
+        foreach(var name in names)
+        {
+            Console.WriteLine(name);
+        }
+        // remove names not larger than 5 characters
+        for(var i = 0; i < names.Count; i++)
+        {
+            if(names[i].Length <= 5)
+                names.RemoveAt(i);
+        }
+        return names;
+        }
     }
 }
