09da3c8646673c43e3eb41d76d68f6a2dbd9ab7a
funki73 <randy.calpe@hotmail.com>
2021-12-02 03:42:23 -0800

    1202210343

--- practice/Program.cs
+++ practice/Program.cs
@@ -7,18 +7,8 @@ namespace practice
     {
         static void Main(string[] args)
         {
-            //Initializing an empty list of Motorcycle Manufacturers
-            List<string> bikes = new List<string>();
-            //Use the Add function in a similar fashion to push
-            bikes.Add("Kawasaki");
-            bikes.Add("Triumph");
-            bikes.Add("BMW");
-            bikes.Add("Moto Guzzi");
-            bikes.Add("Harley Davidson");
-            bikes.Add("Suzuki");
-            //Accessing a generic list value is the same as you would an array
-            Console.WriteLine(bikes[2]); //Prints "BMW"
-            Console.WriteLine($"We currently know of {bikes.Count} motorcycle manufacturers.");
+            int IntegerValue = 65;
+            double DoubleValue = IntegerValue;
         }
     }
 }
