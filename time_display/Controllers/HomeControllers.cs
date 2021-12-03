028c52f84e3eddd40c8e0c8c4447d8641d89a6fa
funki73 <randy.calpe@hotmail.com>
2021-12-03 07:07:17 -0800

    1203210707

---
+++ time_display/Controllers/HomeControllers.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace time_display     //be sure to use your own project's namespace!
+{
+    public class HomeController : Controller   //remember inheritance??
+    {
+        [HttpGet("")]
+        public ViewResult Index()
+        {
+
+            return View();
+        }
+    }
+}
