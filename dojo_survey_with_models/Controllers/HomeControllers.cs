287cc7f42398903c32e4becb96b7fc9929b98c39
funki73 <randy.calpe@hotmail.com>
2021-12-04 20:28:54 -0800

    12040828

---
+++ dojo_survey_with_models/Controllers/HomeControllers.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dojo_survey_with_models
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public ViewResult Index()
+        {
+            return View();
+        }
+        [HttpPost("/survey/submission")]
+        public ViewResult SurveySubmission(string name, string favoritelanguage, string dojolocation, string comments)
+        {
+            ViewBag.Name = name;
+            ViewBag.Favoritelanguage = favoritelanguage;
+            ViewBag.Dojolocation = dojolocation;
+            ViewBag.Comments = comments;
+            return View();
+        }
+    }
+}
