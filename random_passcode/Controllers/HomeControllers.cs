463ee49410c00c11380297439a83e36df9626067
funki73 <randy.calpe@hotmail.com>
2021-12-14 18:57:32 -0800

    1214210657

--- random_passcode/Controllers/HomeControllers.cs
+++ random_passcode/Controllers/HomeControllers.cs
@@ -1,12 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
-using random_passcode.Models;
+
 
 namespace random_passcode
 {
@@ -29,21 +25,20 @@ namespace random_passcode
             int? count = HttpContext.Session.GetInt32("Count");
 
             ViewBag.Count = count;
-            ViewBag.Code = TempData["newCode"];
+            ViewBag.Passwrd = TempData["newCode"];
             // Passcode toRender = new Passcode ((int) count);
 
             GenerateCode();
-            Console.WriteLine(count);
-            return View();
+            return View("Index");
         }
 
         [HttpGet("increment")]
         public IActionResult Increment()
         {
             int? count = HttpContext.Session.GetInt32("Count");
-            count++;
+            count = count++;
             HttpContext.Session.SetInt32("Count", (int) count);
-
+            
             return RedirectToAction("Index");
         }
 
@@ -55,9 +50,9 @@ namespace random_passcode
             char[] charArr = library.ToCharArray();
             Random rand = new Random();
             char[] passArr = new char[14];
-            for(int idx = 0; idx < passArr.Length; idx++)
+            for(int pw = 0; pw < passArr.Length; pw++)
             {
-                passArr[idx] = charArr[rand.Next(35)];
+                passArr[pw] = charArr[rand.Next(35)];
             }
 
             TempData["newCode"] = new string(passArr);
